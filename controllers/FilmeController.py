12341521ee6bf4030c1e23b735a3bed1a8c559bc
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-01-22 19:33:32 -0300

    [FIN] /filme to POST new obj and add to app.include [DEL] app.get in route ' / '

--- controllers/FilmeController.py
+++ controllers/FilmeController.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import ValidationError
 from entities.Filmes import Filme
-from typing import List
 from services.FilmeService import FilmeService
 from fastapi.responses import JSONResponse
 
@@ -10,14 +10,18 @@ filme_router = APIRouter(prefix="/filmes", tags=["Filmes"])
 
 @filme_router.post("/", response_model=Filme)
 def criar_filme(filme: Filme):
-    resultado = filmeService.adicionar_filme_db((filme.titulo, filme.descricao, filme.ano))
-    if "sucesso" in resultado:
+    try:
         return JSONResponse(
-            content={"message": "Filme adicionado com sucesso", "filme": filme.model_dump()},
-            status_code=201  # Código HTTP para criação bem-sucedida
+            content={"message": "Filme adicionado com sucesso","filme": filme.model_dump()},
+            status_code=201
         )
-    else:
-        return JSONResponse(
-            content={"message": resultado},
-            status_code=400  # Código HTTP para erro
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=400,
+            detail="Erro de validação: Os campos 'titulo', 'descricao' e 'ano' precisam ser preenchidos corretamente."
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="Ocorreu um erro inesperado. Por favor, tente novamente mais tarde."
         )
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from interfaces.FilmeDB import Conexao
+from controllers.FilmeController import filme_router
 
-app = FastAPI(title="CRUD de Filmes", version="1.0")
+def lifespan(app: FastAPI):
+    conexao = Conexao()
+    conexao.connect()
+    conexao.create_tables()
+    print("Startup tasks completed")
+    yield
+    print("Shutdown tasks completed")
+    conexao.fechar()
 
-@app.get("/")
-def root():
-    return {"message": "Bem-vindo ao CRUD de Filmes"}
+
+app = FastAPI(title="CRUD de Filmes", version="1.0", lifespan=lifespan)
+
+app.include_router(filme_router)
