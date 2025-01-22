92aa234fe311de44976fbaa3c11199ca540b0fec
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-01-22 19:29:56 -0300

    [ALT] Imp validator to Filme model and update FilmeService to remove redundant checks

--- entities/Filmes.py
+++ entities/Filmes.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
 from typing import Optional
 
 class Filme(BaseModel):
@@ -6,3 +6,9 @@ class Filme(BaseModel):
     titulo: str
     descricao: str
     ano: int
+
+    @model_validator(mode='before')
+    def check_required_fields(cls, values):
+        if not values.get('titulo') or not values.get('descricao') or not values.get('ano'):
+            raise ValueError("Os campos 'titulo', 'descricao' e 'ano' precisam ser preenchidos corretamente.")
+        return values
--- services/FilmeService.py
+++ services/FilmeService.py
@@ -1,3 +1,4 @@
+from pydantic import ValidationError
 from interfaces.FilmeDB import Conexao
 from typing import Tuple
 class FilmeService:
@@ -5,19 +6,18 @@ class FilmeService:
     @staticmethod
     def adicionar_filme_db(values: Tuple[str, str, int]):
         titulo, descricao, ano = values
-        if not titulo or not descricao or not ano:
-            return "Os campos 'titulo', 'descricao' e 'ano' precisam ser preenchidos corretamente."
-        else:
-            try:    
-                conexao = Conexao()
-                sql = ("INSERT INTO Filme (titulo, descricao, ano) VALUES (?, ?, ?) ")
-                params = (titulo, descricao, ano)
-                if conexao.gravar(sql, params):
-                    return "sucesso"
-                else:
-                    return "Erro ao gravar no banco."
-            except Exception as e:
-                return f'Erro ao salvar o filme: {str(e)}'
+        try:    
+            conexao = Conexao()
+            sql = ("INSERT INTO Filme (titulo, descricao, ano) VALUES (?, ?, ?) ")
+            params = (titulo, descricao, ano)
+            if conexao.gravar(sql, params):
+                return "sucesso"
+            else:
+                return "Erro ao gravar no banco."
+        except Exception as e:
+            return f'Erro ao salvar o filme: {str(e)}'
+    
+                
                 
             
                 
