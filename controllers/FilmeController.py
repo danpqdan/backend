f51ee8b8a60c4dcc6ab89c00da9380c394269dab
ds-ubunto@desktop <danieltisantos@gmail.com>
2025-01-22 20:40:04 -0300

    [FIN] /filmes[] to Controller and Service return

--- controllers/FilmeController.py
+++ controllers/FilmeController.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, HTTPException
 from pydantic import ValidationError
 from entities.Filmes import Filme
@@ -8,19 +9,34 @@ filmeService = FilmeService()
 
 filme_router = APIRouter(prefix="/filmes", tags=["Filmes"])
 
+@filme_router.get("", response_model=List[Filme])
+def retornar_filmes():
+    try:
+        filmes = filmeService.listar_filmes()
+        return JSONResponse(
+            content={"Filmes: ": filmes},
+            status_code=200 
+        )
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Ocorreu um erro inesperado. Por favor, tente novamente mais tarde."
+        )
+
 @filme_router.post("", response_model=Filme)
 def criar_filme(filme: Filme):
     try:
+        filmeService.adicionar_filme((filme.titulo, filme.descricao, filme.ano))
         return JSONResponse(
             content={"message": "Filme adicionado com sucesso","filme": filme.model_dump(exclude={"id"})},
             status_code=201
         )
-    except ValidationError as e:
+    except ValidationError:
         raise HTTPException(
             status_code=400,
             detail="Erro de validação: Os campos 'titulo', 'descricao' e 'ano' precisam ser preenchidos corretamente."
         )
-    except Exception as e:
+    except Exception:
         raise HTTPException(
             status_code=500,
             detail="Ocorreu um erro inesperado. Por favor, tente novamente mais tarde."
--- services/FilmeService.py
+++ services/FilmeService.py
@@ -1,10 +1,24 @@
 from pydantic import ValidationError
+from entities.Filmes import Filme
 from interfaces.FilmeDB import Conexao
-from typing import Tuple
+from typing import List, Tuple
 class FilmeService:
 
     @staticmethod
-    def adicionar_filme_db(values: Tuple[str, str, int]):
+    def listar_filmes() -> List[Filme]:
+        try:
+            conexão = Conexao()
+            sql = "SELECT * FROM Filme"
+            filmes = conexão.consultar_lista(sql)
+            if not filmes:
+                return []
+            return filmes
+        except Exception as e:
+            return f'Erro ao consultar lista de filme: {str(e)}'
+
+
+    @staticmethod
+    def adicionar_filme(values: Tuple[str, str, int]):
         titulo, descricao, ano = values
         try:    
             conexao = Conexao()
@@ -15,7 +29,7 @@ class FilmeService:
             else:
                 return "Erro ao gravar no banco."
         except Exception as e:
-            return f'Erro ao salvar o filme: {str(e)}'
+            return f'Erro ao salvar o filme: {str(e)}'                
     
                 
                 
